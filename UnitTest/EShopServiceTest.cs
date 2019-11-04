2f73c5b031993047f1124e7a101239f3f5e68e30
Pent <robzaha@hotmail.com>
2019-11-05 00:08:57 +0100

    UnitTest Init

---
+++ UnitTest/EShopServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using DataLayer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class EShopServiceTest
+    {
+        [TestMethod]
+        public void Add_New_Customer()
+        {
+            var options = new DbContextOptionsBuilder<EShopContext>()
+                .UseInMemoryDatabase(databaseName: "EShopDb_InMemory")
+                .Options;
+
+            //using(var context = new EShopContext(options))
+            //{
+            //    var productService = new ListProductService(context);
+            //    var dropdownItems = ProductFilterDropdownService.GetFilterDropDownValues(ProductFilterBy.ByBrand).ToList();
+            //}
+        }
+    }
+}
