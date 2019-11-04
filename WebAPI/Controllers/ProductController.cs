98bce7974121cdf6a61c15ef0162efa9d06277e3
Pent <robzaha@hotmail.com>
2019-11-05 00:37:22 +0100

    API for products done.

---
+++ WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using ServiceLayer.ProductService.Concrete;
+using ServiceLayer.ProductService.ProductDto;
+using ServiceLayer.ProductService.QueryObjects;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/Products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ListProductService _productService;
+
+        public ProductController(ListProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductListDto>>> GetProducts()
+        {
+            ProductListSortOptions Option = new ProductListSortOptions();
+            return await _productService.SortFilterPage(Option).ToListAsync();
+        }
+
+        [HttpGet("{OrderBy}")]
+        public async Task<ActionResult<IEnumerable<ProductListDto>>> GetProductByOrder(OrderByOptions OrderBy)
+        {
+            ProductListSortOptions Option = new ProductListSortOptions()
+            {
+                OrderByOptions = OrderBy    //Brand, Color, HighPrice, LowPrice, MaterialFrameType, Model, SimpleOrder, Storage, Year
+            };
+
+            return await _productService.SortFilterPage(Option).ToListAsync();
+        }
+
+        [HttpGet("{OrderBy}/{FilterBy}")]
+        public async Task<ActionResult<IEnumerable<ProductListDto>>> GetProductByOrderAndFilterBy(OrderByOptions OrderBy, ProductFilterBy FilterBy)
+        {
+            ProductListSortOptions Option = new ProductListSortOptions()
+            {
+                OrderByOptions = OrderBy,    //Brand, Color, HighPrice, LowPrice, MaterialFrameType, Model, SimpleOrder, Storage, Year
+                FilterBy = FilterBy          //ByBrand, ByMaterialFrameType, NoFilter
+            };
+
+            return await _productService.SortFilterPage(Option).ToListAsync();
+        }
+
+        [HttpGet("{OrderBy}/{FilterBy}/{FilterValue}")]
+        public async Task<ActionResult<IEnumerable<ProductListDto>>> GetProductByFullCustom(OrderByOptions OrderBy, ProductFilterBy FilterBy, string FilterValue)
+        {
+            ProductListSortOptions Option = new ProductListSortOptions()
+            {
+                OrderByOptions = OrderBy,    //Brand, Color, HighPrice, LowPrice, MaterialFrameType, Model, SimpleOrder, Storage, Year
+                FilterBy = FilterBy,         //ByBrand, ByMaterialFrameType, NoFilter
+                FilterValue = FilterValue    //Input Value
+            };
+
+            return await _productService.SortFilterPage(Option).ToListAsync();
+        }
+    }
+}
---
+++ WebAPI/Startup.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataLayer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ServiceLayer.ProductService.Concrete;
+
+namespace WebAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<EShopContext>();
+            services.AddScoped<ListProductService>();
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
