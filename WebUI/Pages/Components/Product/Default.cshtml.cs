11e5cd924d1962b7e47e769ae3897c15b2ef2b8a
Pent <robzaha@hotmail.com>
2019-11-05 14:01:43 +0100

    Better new design.

---
+++ WebUI/Pages/Components/Product/Default.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebUI.Pages.Components.Product
+{
+    public class DefaultModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ WebUI/ViewComponents/ProductViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceLayer.ProductService.Concrete;
+using ServiceLayer.ProductService.ProductDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.ViewComponents
+{
+    public class ProductViewComponent : ViewComponent
+    {
+        private readonly ListProductService _productService;
+
+        public ProductViewComponent(ListProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ProductListSortOptions Option = new ProductListSortOptions();
+            var products = _productService.SortFilterPage(Option).ToList().Count();
+            return View(products);
+        }
+    }
+}
